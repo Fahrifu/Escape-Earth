d58e2353645b5877d81b24972e77cc0fd859ef88
fahrifu <fahrifu@outlook.com>
2025-03-10 11:48:44 +0100

    Solve sunradius challenge function

--- Escape_Earth_Script.mjs
+++ Escape_Earth_Script.mjs
@@ -15,7 +15,7 @@ const SOLAR_SYSTEM_API = "https://api.le-systeme-solaire.net/rest";
 async function startChallenge() {
     try {
         console.log("Connecting to RIS System...");
-        const response = await fetch (`${RIS_URL}/start?player=${PLAYER}`);
+        const response = await fetch(`${RIS_URL}/start?player=${PLAYER}`);
         const data = await response.json();
         console.log("Data Received:", data);
 
@@ -23,13 +23,55 @@ async function startChallenge() {
 
         // Check what type of challenge we have received and solve accordingly
         if (challenge.includes("equatorial radius")) {
-            
+            await solveSunRadiusChallenge();
         }
     } catch (error) {
         console.error("Error starting mission:", error);
     }
 }
 
+/**
+ * Solves the challenge related to the Sun's radius by finding the PIN
+ */
+async function solveSunRadiusChallenge() {
+    try {
+        console.log("Fetching data...") 
+        const response = await fetch(`${SOLAR_SYSTEM_API}/bodies/soleil`);
+        const sunData = await response.json();
+
+        // Extract equatorial radius and mean radius
+        const equatorialRadius = sunData.equaRadius;
+        const meanRadius = sunData.meanRadius;
+
+        // Compute the access PIN by subtracting the mean radius from the equatorial radius
+        const accessPin = equatorialRadius - meanRadius;
+
+        console.log(`Equatorial Radius: ${equatorialRadius} km`);
+        console.log(`Mean Radius: ${meanRadius} km`);
+        console.log(`Calcualted Access Pin: ${accessPin}`);
+
+        //Submitting the computed answer
+        const result = await submitAnswer(accessPin);
+        console.log("Response from RIS:", result);
+
+        //If a new challenge is presented, proceed to the next step
+        if (result.nextChallenge) {
+
+        }
+
+        //If a skeleton key is received, save it
+        if (result.skeletonKey) {
+            
+        }
+    } catch (error) {
+        console.error(" Error solving sun radius challenge: ", error);
+    }
+}
+
+/**
+ * Submits an answer to the RIS system
+ * @param {string|number} answer - The computed answer to submit  
+ */
 async function submitAnswer(answer) {
     try {
         console.log("Submitting answer...");
@@ -43,3 +85,4 @@ async function submitAnswer(answer) {
         console.error("Error submit answer:", error);
     }
 }
+
