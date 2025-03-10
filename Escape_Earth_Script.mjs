cbd9e0561343c26bc813ef042372e87f0dfe1e24
fahrifu <fahrifu@outlook.com>
2025-03-10 11:16:25 +0100

    Submit answer demo

--- Escape_Earth_Script.mjs
+++ Escape_Earth_Script.mjs
@@ -7,6 +7,11 @@ const PLAYER = "hlmnguyen@uia.no";
 const RIS_URL = "https://spacescavanger.onrender.com";
 const SOLAR_SYSTEM_API = "https://api.le-systeme-solaire.net/rest";
 
+/**
+ * Initiates the mission by contacting the RIS system
+ * Determines the challenge type of executes the corresponding function
+ */
+
 async function startChallenge() {
     try {
         console.log("Connecting to RIS System...");
@@ -25,3 +30,16 @@ async function startChallenge() {
     }
 }
 
+async function submitAnswer(answer) {
+    try {
+        console.log("Submitting answer...");
+        const response = await fetch(`${RIS_URL}/answer`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ answer: answer, player: PLAYER})
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error submit answer:", error);
+    }
+}
