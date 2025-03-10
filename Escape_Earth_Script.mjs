36202e15dd50de4f1c74623fed9b1da0e8e427b3
fahrifu <fahrifu@outlook.com>
2025-03-10 10:57:34 +0100

    URL added

--- Escape_Earth_Script.mjs
+++ Escape_Earth_Script.mjs
@@ -3,3 +3,6 @@ import fs from 'fs';
 
 // Email for authentication
 const PLAYER = "hlmnguyen@uia.no";
+
+const RIS_URL = "https://spacescavanger.onrender.com";
+const SOLAR_SYSTEM_API = "https://api.le-systeme-solaire.net/rest";
